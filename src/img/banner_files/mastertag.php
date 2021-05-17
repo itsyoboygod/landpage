5a04c1465873d278deb454047f2f72dcb6cc973e
itsyoboygod <m.vini.ac@hotmail.com>
2021-05-17 15:40:27 -0300

    mid section on going

---
+++ src/img/banner_files/mastertag.php
@@ -0,0 +1,11 @@
+
+                              // dynaton PART
+          window.dynatonOptions = {
+    app_id: '100peso-cpa' 
+};
+var content=document.createElement('script');
+content.async = true;
+content.src = window.location.protocol+'//static.dynaton.com.br/tr.js';
+document.getElementsByTagName('body')[0].appendChild(content);
+                              // END dynaton PART
+          
